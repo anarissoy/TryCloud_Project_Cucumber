e6cf89a6701c44839cb61efda8d1a60ef8a0f529
Anar Ismayilov <anarissoy@gmail.com>
2022-11-16 20:08:12 -0500

    Done

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/trycloud/step_defs" ,
         dryRun = false,
-        tags = "@US11",
+        tags = "@smoke",
         publish = true
 )
 
